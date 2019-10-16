2374650ecda57b79b1b066802e1c9cd59551e3b0
Mahdi Najjarian <mnajarian@gmail.com>
2019-10-16 13:27:14 +0300

    add user

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,12 +1,41 @@
-import * as mongoose from 'mongoose';
+import { Schema, Document, Model, model } from 'mongoose';
+import * as bcrypt from 'bcrypt';
+import * as passportLocalMongoose from 'passport-local-mongoose';
+import { IUser } from '../interfaces/user';
 
-const userSchema = new mongoose.Schema({
-    admin: {
-        type: Boolean,
-        default: false
+export interface IUserModel extends IUser, Document {
+    fullName(): string;
+}
+
+export const UserSchema: Schema = new Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    avatar: {
+        type: String
+    },
+    about: {
+        type: String,
+        trim: true
     }
-})
+});
 
-const User = mongoose.model('User', userSchema);
+UserSchema.methods.fullName = function(): string  {
+    return (this.firstName.trim() + " " + this.lastName.trim());
+}
+
+UserSchema.plugin(passportLocalMongoose, { usernameField: 'email'});
+
+const User = model<IUserModel>('User', UserSchema);
 
 export default User;
