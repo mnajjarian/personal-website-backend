dbee6ed39a435629e21d15172badfeb7fccc0ffd
Mahdi Najjarian <mnajarian@gmail.com>
2019-10-29 21:53:17 +0200

    serve static

--- src/server.ts
+++ src/server.ts
@@ -1,43 +1,53 @@
-import * as express from 'express';
-import { config } from 'dotenv';
-import * as bodyParser from 'body-parser';
-import * as cookieParser from 'cookie-parser';
-import * as mongoose from 'mongoose';
-import * as morgan from 'morgan';
-import * as session from 'express-session'; 
-import * as passport from 'passport';
-import * as cors from 'cors';
-import * as helmet from 'helmet';
-import router from './routes/index';
+import * as express from "express";
+import { config } from "dotenv";
+import * as bodyParser from "body-parser";
+import * as cookieParser from "cookie-parser";
+import * as mongoose from "mongoose";
+import * as morgan from "morgan";
+import * as session from "express-session";
+import * as passport from "passport";
+import * as cors from "cors";
+import * as helmet from "helmet";
+import router from "./routes/index";
 
 const app = express();
-app.use(helmet())
-app.use(cors({
-    origin: '*',
-    credentials: true,
-}));
-if (process.env.NODE_ENV !== 'production') {
+
+app.use(helmet());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true
+  })
+);
+if (process.env.NODE_ENV !== "production") {
   config();
 }
 
+mongoose.set("useCreateIndex", true);
 mongoose
-  .connect(process.env.MONGODB_URI || '', { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.MONGODB_URI || "", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
   .then(() => {
-    console.log('connected to database ', process.env.MONGODB_URI);
+    console.log("connected to database ", process.env.MONGODB_URI);
     return null;
-  }).catch((err: Error) => {
+  })
+  .catch((err: Error) => {
     console.log(err);
     return null;
   });
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(passport.initialize());
-app.use(passport.session())
-require('./passportConfig');
+app.use(passport.session());
+require("./passportConfig");
 
-app.use('/', router);
+app.use("/", router);
+
+app.use("/public", express.static("public"));
 
 const port = process.env.PORT;
 app.listen(port, () => {
