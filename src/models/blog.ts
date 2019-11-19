336595fc7017c222db4e3e5cbc1194df2fdd8151
mnajjarian <mnajarian@gmail.com>
2019-11-19 16:45:40 +0200

    add author

--- src/models/blog.ts
+++ src/models/blog.ts
@@ -1,21 +1,28 @@
-import { Schema, Document, model } from 'mongoose';
-import { Timestamp } from 'bson';
+import { Schema, Document, model } from "mongoose";
+import { Timestamp } from "bson";
 
 export interface Blog extends Document {
-    content: string;
-};
+  content: string;
+  author: string;
+}
 
-const BlogSchema = new Schema({
+const BlogSchema = new Schema(
+  {
     content: {
-        type: String,
+      type: String
+    },
+    author: {
+      type: String
     }
-},{
+  },
+  {
     timestamps: {
-        createdAt: true,
-        updatedAt: true
+      createdAt: true,
+      updatedAt: true
     }
-})
+  }
+);
 
-const Blog = model<Blog>('Blog', BlogSchema);
+const Blog = model<Blog>("Blog", BlogSchema);
 
 export default Blog;
