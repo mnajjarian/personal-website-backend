cd8a3a7c67b30c4b4ab5f8dbbc6fb2c4abfbc578
mnajjarian <mnajarian@gmail.com>
2019-12-09 13:01:20 +0200

    add comment route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,11 +4,14 @@ import { BlogController } from '../controllers/blogController';
 import { UserController } from '../controllers/userController';
 import { ProfileController } from '../controllers/profileController';
 import { GalleryController } from '../controllers/galleryController';
+import { CommentController } from '../controllers/commentController';
 
 const userController = new UserController();
 const blogController = new BlogController();
+const commentController = new CommentController();
 const profileController = new ProfileController();
 const gallaryController = new GalleryController();
+
 const router = Router();
 
 router.get('/users', userController.getUsers)
@@ -20,6 +23,9 @@ router.post('/posts', blogController.create)
 router.get('/posts', blogController.getPosts)
 router.put('/posts/:id', blogController.updatePost)
 router.delete('/posts/:id', blogController.removePost)
+
+router.get('/comments', commentController.getComments)
+router.post('/comments', commentController.addComment)
 
 router.post('/profile', profileController.create)
 router.get('/profile', profileController.getProfile)
