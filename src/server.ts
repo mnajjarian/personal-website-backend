9322fc7baf48611f8158a5ae861c1acb0d112b6e
Mahdi Najjarian <mnajarian@gmail.com>
2020-03-12 12:16:57 +0200

    uibuild

--- src/server.ts
+++ src/server.ts
@@ -30,7 +30,7 @@ app.use(
     store: sessionStore,
     cookie: {
       maxAge: 1000 * 60 * 60 * 24,
-      httpOnly: true,
+      //httpOnly: true,
       //sameSite: true,
       secure: true
     }
